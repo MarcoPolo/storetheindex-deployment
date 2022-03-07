bde2cb52011c2f828fa0db477d572cc47dca4e63
Marco Munizaga <git@marcopolo.io>
2022-03-07 11:57:03 -0800

    Use locking

--- backend.tf
+++ backend.tf
@@ -1,8 +1,9 @@
 terraform {
   backend "s3" {
-    profile = "storetheindex"
-    bucket  = "load-testing-tfstate"
-    key     = "indexer-state"
-    region  = "us-west-2"
+    profile        = "storetheindex"
+    bucket         = "load-testing-tfstate"
+    key            = "indexer-state"
+    region         = "us-west-2"
+    dynamodb_table = "terraform-locking"
   }
 }
